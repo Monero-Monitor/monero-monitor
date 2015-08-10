05950d1570787e256c291e540578956580b8e416
Mike C <croteaumj@Michael-MBP.local>
2015-08-09 22:11:55 -0400

    Release setup.

---
+++ build-chrome.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "\nSyncing chrome directory...\n===========================\n"
+rsync -a --exclude-from=./exclude-chrome.txt . build/chrome/.
+
+echo "\nZipping chrome extension...\n===========================\n"
+mv build/chrome.zip build/chrome-old.zip
+zip -r build/chrome.zip build/chrome
+
+echo "\n========\n > Extension located in ./build/chrome.zip\n"
